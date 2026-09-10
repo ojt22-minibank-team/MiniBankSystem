164211d294623c90683c6f6f9b3fdcd7019a052f
ZinMinHtet <zmh430412@gmail.com>
2026-09-10 21:07:06 +0630

    Unknow

---
+++ corebanking/src/main/java/com/corebanking/repository/RolesRepository.java
@@ -0,0 +1,18 @@
+package com.corebanking.repository;
+
+import com.corebanking.entity.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RolesRepository extends JpaRepository<Roles, Integer> {
+
+	@Query("SELECT r FROM Roles r WHERE r.role_code = :roleCode")
+	Optional<Roles> findByRole_code(@Param("roleCode") String roleCode);
+
+	@Query(value = "SELECT r.role_code FROM roles r JOIN staff_user_roles sur ON r.role_id = sur.role_id WHERE sur.staff_id = :staffId", nativeQuery = true)
+	List<String> findRoleCodesByStaffId(@Param("staffId") Long staffId);
+}
