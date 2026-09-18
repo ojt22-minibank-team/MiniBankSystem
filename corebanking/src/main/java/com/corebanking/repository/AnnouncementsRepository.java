d19891f158bcaf2fdbe008706c33456be4b7b4a7
ZinMinHtet <zmh430412@gmail.com>
2026-09-18 12:00:33 +0630

    .

--- corebanking/src/main/java/com/corebanking/repository/AnnouncementsRepository.java
+++ corebanking/src/main/java/com/corebanking/repository/AnnouncementsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.corebanking.entity.Announcements;
 
-public interface AnnouncementsRepository
-extends JpaRepository<Announcements, Long> {
+public interface AnnouncementsRepository 
+
+          extends JpaRepository<Announcements, Long> {
 }
